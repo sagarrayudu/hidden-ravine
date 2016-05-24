48157d40d932f5c783ec06fafb1528409abf64f1
sagar12861 <krishnasagar.r@sonata-software.com>
2016-05-24 12:27:21 +0530

    Added webpack and bootstrap

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var app = express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/build'));
 
 // set the home page route
 app.get('/', function(req, res) {
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import './sass/main.scss';
+import 'style!raw!bootstrap/dist/css/bootstrap.css';
+
+global.jQuery = require('jquery');
+require('bootstrap');
